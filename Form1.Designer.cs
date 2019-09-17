7b069c640c4a5dfe6e3321c5a8cab307966fac4c
iSTALK3R <36308128+iSTALK3R@users.noreply.github.com>
2019-09-16 22:03:20 -0300

    First Upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace prjCadastro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabConsultar = new System.Windows.Forms.TabPage();
+            this.tabCadastro = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.txtRG = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabCadastro.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabConsultar);
+            this.tabControl1.Controls.Add(this.tabCadastro);
+            this.tabControl1.Location = new System.Drawing.Point(1, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(657, 306);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabConsultar
+            // 
+            this.tabConsultar.BackColor = System.Drawing.SystemColors.Control;
+            this.tabConsultar.Location = new System.Drawing.Point(4, 22);
+            this.tabConsultar.Name = "tabConsultar";
+            this.tabConsultar.Padding = new System.Windows.Forms.Padding(3);
+            this.tabConsultar.Size = new System.Drawing.Size(603, 280);
+            this.tabConsultar.TabIndex = 0;
+            this.tabConsultar.Text = "Consultar";
+            // 
+            // tabCadastro
+            // 
+            this.tabCadastro.BackColor = System.Drawing.SystemColors.Control;
+            this.tabCadastro.Controls.Add(this.btnGravar);
+            this.tabCadastro.Controls.Add(this.btnCancelar);
+            this.tabCadastro.Controls.Add(this.txtEmail);
+            this.tabCadastro.Controls.Add(this.txtCelular);
+            this.tabCadastro.Controls.Add(this.txtTelefone);
+            this.tabCadastro.Controls.Add(this.txtBairro);
+            this.tabCadastro.Controls.Add(this.txtEndereco);
+            this.tabCadastro.Controls.Add(this.txtRG);
+            this.tabCadastro.Controls.Add(this.txtCPF);
+            this.tabCadastro.Controls.Add(this.txtNome);
+            this.tabCadastro.Controls.Add(this.label8);
+            this.tabCadastro.Controls.Add(this.label7);
+            this.tabCadastro.Controls.Add(this.label6);
+            this.tabCadastro.Controls.Add(this.label5);
+            this.tabCadastro.Controls.Add(this.label4);
+            this.tabCadastro.Controls.Add(this.label3);
+            this.tabCadastro.Controls.Add(this.label2);
+            this.tabCadastro.Controls.Add(this.label1);
+            this.tabCadastro.Location = new System.Drawing.Point(4, 22);
+            this.tabCadastro.Name = "tabCadastro";
+            this.tabCadastro.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCadastro.Size = new System.Drawing.Size(649, 280);
+            this.tabCadastro.TabIndex = 1;
+            this.tabCadastro.Text = "Cadastrar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(257, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(443, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "RG";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Endereço";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(257, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Bairro";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(443, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Telefone";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(16, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Celular";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(257, 137);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Email";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(20, 45);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(221, 22);
+            this.txtNome.TabIndex = 8;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCPF.Location = new System.Drawing.Point(261, 45);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(168, 22);
+            this.txtCPF.TabIndex = 9;
+            // 
+            // txtRG
+            // 
+            this.txtRG.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRG.Location = new System.Drawing.Point(446, 45);
+            this.txtRG.Name = "txtRG";
+            this.txtRG.Size = new System.Drawing.Size(182, 22);
+            this.txtRG.TabIndex = 10;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEndereco.Location = new System.Drawing.Point(20, 101);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(221, 22);
+            this.txtEndereco.TabIndex = 11;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBairro.Location = new System.Drawing.Point(261, 101);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(168, 22);
+            this.txtBairro.TabIndex = 12;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.Location = new System.Drawing.Point(447, 101);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(181, 22);
+            this.txtTelefone.TabIndex = 13;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCelular.Location = new System.Drawing.Point(20, 160);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(221, 22);
+            this.txtCelular.TabIndex = 14;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(261, 160);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(367, 22);
+            this.txtEmail.TabIndex = 15;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.Red;
+            this.btnCancelar.Location = new System.Drawing.Point(350, 213);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(144, 38);
+            this.btnCancelar.TabIndex = 16;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.BackColor = System.Drawing.SystemColors.Control;
+            this.btnGravar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGravar.FlatAppearance.BorderColor = System.Drawing.Color.LimeGreen;
+            this.btnGravar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
+            this.btnGravar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGravar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGravar.ForeColor = System.Drawing.Color.LimeGreen;
+            this.btnGravar.Location = new System.Drawing.Point(182, 213);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(144, 38);
+            this.btnGravar.TabIndex = 17;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = false;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 310);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabCadastro.ResumeLayout(false);
+            this.tabCadastro.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabConsultar;
+        private System.Windows.Forms.TabPage tabCadastro;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtRG;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjCadastro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string ValidarCPF(string sCPF)
+        {
+            // Criar variavel do tipo array
+            int[] arrayCPF = new int[9];
+            // Criar variavel do tipo array para armazenar 10 valores
+            int[] arrayCPF2 = new int[10];
+            // Criar variavel para armazenar o cpf tratado
+            string sCPFTratado = "";
+            // Criar variavel para armazenar o valor da soma
+            int iValorSoma = 0;
+            // Criar variavel para armazwnar o valor da soma do segundo digito
+            int iValorSoma2 = 0;
+            // Criar variavel para calcular a multiplicação
+            int iValorMult = 0;
+            // Criar variavel para calcular a multiplicação do segundo digito
+            int iValorMult2 = 0;
+            // Criar variavel para armazenar o valor do primeiro digito
+            int iPrimeiroDigito = 0;
+            // Criar variavel para armazenar o valor do segundo digito
+            int iSegundoDigito = 0;
+            // Variavel para armazenar o penultimo digito digitado
+            int iPrimeiroValor = 0;
+            // Variavel para armazenar o ultimo digito digitado
+            int iSegundoValor = 0;
+
+
+            // Trata a string do CPF
+            sCPFTratado = sCPF.Replace(".", "").Replace("-", "").Replace("/", "");
+
+            // Criar um For para preencher o array
+            for (int i = 0; i < 9; i++)
+            {
+                // Passando o valor da variavel para o array
+                arrayCPF[i] = Convert.ToInt32(sCPFTratado.Substring(i, 1)) * (10 - i);
+            }
+
+            // Fazer for para somar os valores do array
+            for (int i = 0; i < 9; i++)
+            {
+                // Somar os valores e passar para a variavel
+                iValorSoma += arrayCPF[i];
+            }
+
+            // Calcular a multiplicação por 10
+            iValorMult = iValorSoma * 10;
+            // Armazenar o valor do resto da divisão
+            iPrimeiroDigito = iValorMult % 11;
+
+            // Criar for para preencher o Array com 10 valores
+            for (int i = 0; i < 10; i++)
+            {
+                arrayCPF2[i] = Convert.ToInt32(sCPFTratado.Substring(i, 1)) * (11 - i);
+            }
+
+            // Criar for para somar os valores do segundo digito
+            for (int i = 0; i < 10; i++)
+            {
+                iValorSoma2 += arrayCPF2[i];
+            }
+
+            // Calculando a multiplicação por 10
+            iValorMult2 = iValorSoma2 * 10;
+            // Armazenano o resto da divisão
+            iSegundoDigito = iValorMult2 % 11;
+
+            iPrimeiroValor = Convert.ToInt32(sCPFTratado.Substring(9, 1));
+            iSegundoValor = Convert.ToInt32(sCPFTratado.Substring(10, 1));
+
+
+            if (iPrimeiroValor == iPrimeiroDigito && iSegundoValor == iSegundoDigito)
+            {
+                return "CPF Valido";
+
+            }
+            else
+            {
+                return "CPF Invalido";
+            }
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(ValidarCPF(txtCPF.Text).ToString());
+        }
+    }
+}
